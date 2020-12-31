7dcdf15431aef1a87e75baf8d2c9f3f41c32d5cd
hongdong <isguohongdong@gmail.com>
2020-12-31 13:31:57 +0800

    解析数据生成文件

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,48 @@
 import superagent from 'superagent'
 import cheerio from 'cheerio'
+import fs from 'fs'
+import path from 'path'
 
 interface HouseInfo {
     title: string,
-    follow: string
+    address: string,
+    floor: string,
+    size: string,
+    direction: string,
+    follow: number,
+    time: string,
+}
+
+interface HouseResult {
+    time: number,
+    data: HouseInfo[]
+}
+
+export interface Content {
+    [propName: number]: HouseInfo[];
 }
 class Spider {
     private url = 'https://hz.ke.com/ershoufang/'
-    getHouseInfo(html: string) {
+    getHouseInfo(html: string): HouseResult {
         let houses: HouseInfo[] = []
         const $ = cheerio.load(html)
         $('li.clear').map((index, element) => {
             let title = $(element).find('a.maidian-detail').text().trim()
-            let follow = $(element).find('div.followInfo').text().split('/')[0].trim()
+            let address = $(element).find('.positionInfo a').text().trim()
+            let houseInfos = $(element).find('.houseInfo').text().split('|')
+            let floor = houseInfos[0].trim().replace(/\s+/g, '')
+            let size = houseInfos[1].trim()
+            let direction = houseInfos[2].trim()
+            let follow = +$(element).find('div.followInfo').text().split('/')[0].trim().split('人')[0]
+            let time = $(element).find('div.followInfo').text().split('/')[1].trim()
             houses.push({
                 title,
-                follow
+                address,
+                floor,
+                size,
+                direction,
+                follow,
+                time
             })
         })
         return {
@@ -23,12 +50,28 @@ class Spider {
             data: houses
         }
     }
+    createJSONFile(houseInfo: HouseResult): Content {
+        const filePath = path.resolve(__dirname, '../data/house.json')
+        let fileContent: Content = {}
+        if (fs.existsSync(filePath)) {
+            fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+        }
+        fileContent[houseInfo.time] = houseInfo.data
+        return fileContent
+    }
     async getRawHtml() {
         let result = await superagent.get(this.url)
-        this.getHouseInfo(result.text)
+        return result.text
+    }
+    async initSpiderProcess() {
+        const filePath = path.resolve(__dirname, '../data/house.json')
+        const html = await this.getRawHtml()
+        const houses = this.getHouseInfo(html)
+        const fileContent = this.createJSONFile(houses)
+        fs.writeFileSync(filePath, JSON.stringify(fileContent))
     }
     constructor() {
-        this.getRawHtml()
+        this.initSpiderProcess()
     }
 }
 const spider = new Spider()
