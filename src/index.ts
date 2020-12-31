9f56fc30be91dd5b49f29dca98e956a541402035
hongdong <isguohongdong@gmail.com>
2020-12-31 14:36:42 +0800

    提取分析器

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,30 @@
 import superagent from 'superagent'
-import cheerio from 'cheerio'
-import fs from 'fs'
 import path from 'path'
+import fs from 'fs'
+import keAnalyzer from './keAnalyzer'
 
-interface HouseInfo {
-    title: string,
-    address: string,
-    floor: string,
-    size: string,
-    direction: string,
-    follow: number,
-    time: string,
-}
-
-interface HouseResult {
-    time: number,
-    data: HouseInfo[]
-}
-
-export interface Content {
-    [propName: number]: HouseInfo[];
+export interface Analyzer {
+    analyze: (html: string, filePath: string) => string
 }
 class Spider {
-    private url = 'https://hz.ke.com/ershoufang/'
-    getHouseInfo(html: string): HouseResult {
-        let houses: HouseInfo[] = []
-        const $ = cheerio.load(html)
-        $('li.clear').map((index, element) => {
-            let title = $(element).find('a.maidian-detail').text().trim()
-            let address = $(element).find('.positionInfo a').text().trim()
-            let houseInfos = $(element).find('.houseInfo').text().split('|')
-            let floor = houseInfos[0].trim().replace(/\s+/g, '')
-            let size = houseInfos[1].trim()
-            let direction = houseInfos[2].trim()
-            let follow = +$(element).find('div.followInfo').text().split('/')[0].trim().split('人')[0]
-            let time = $(element).find('div.followInfo').text().split('/')[1].trim()
-            houses.push({
-                title,
-                address,
-                floor,
-                size,
-                direction,
-                follow,
-                time
-            })
-        })
-        return {
-            time: new Date().getTime(),
-            data: houses
-        }
-    }
-    createJSONFile(houseInfo: HouseResult): Content {
-        const filePath = path.resolve(__dirname, '../data/house.json')
-        let fileContent: Content = {}
-        if (fs.existsSync(filePath)) {
-            fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-        }
-        fileContent[houseInfo.time] = houseInfo.data
-        return fileContent
-    }
+    private filePath = path.resolve(__dirname, '../data/house.json')
+
     async getRawHtml() {
-        let result = await superagent.get(this.url)
+        let result = await superagent.get(url)
         return result.text
     }
-    async initSpiderProcess() {
-        const filePath = path.resolve(__dirname, '../data/house.json')
-        const html = await this.getRawHtml()
-        const houses = this.getHouseInfo(html)
-        const fileContent = this.createJSONFile(houses)
-        fs.writeFileSync(filePath, JSON.stringify(fileContent))
+    writeFile(content: string) {
+        fs.writeFileSync(this.filePath, content)
     }
-    constructor() {
+    async initSpiderProcess() {
+        const html = await this.getRawHtml()
+        const fileContent = this.analyze.analyze(html, this.filePath)
+        this.writeFile(fileContent)
+    }
+    constructor(private url: string, private analyze: Analyzer) {
         this.initSpiderProcess()
     }
 }
-const spider = new Spider()
+const url = 'https://hz.ke.com/ershoufang/'
+const analyze = new keAnalyzer()
+const spider = new Spider(url, analyze)
---
+++ src/keAnalyzer.ts
@@ -0,0 +1,67 @@
+import cheerio from 'cheerio'
+import fs from 'fs'
+import { Analyzer } from './index'
+
+export interface HouseInfo {
+    title: string,
+    address: string,
+    floor: string,
+    size: string,
+    direction: string,
+    follow: number,
+    time: string,
+}
+
+export interface HouseResult {
+    time: number,
+    data: HouseInfo[]
+}
+
+export interface Content {
+    [propName: number]: HouseInfo[];
+}
+
+export default class KeAnalyzer implements Analyzer {
+    constructor() {
+    }
+    getJSONInfos(houseInfo: HouseResult, filePath: string): Content {
+        let fileContent: Content = {}
+        if (fs.existsSync(filePath)) {
+            fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+        }
+        fileContent[houseInfo.time] = houseInfo.data
+        return fileContent
+    }
+    getHouseInfo(html: string): HouseResult {
+        let houses: HouseInfo[] = []
+        const $ = cheerio.load(html)
+        $('li.clear').map((index, element) => {
+            let title = $(element).find('a.maidian-detail').text().trim()
+            let address = $(element).find('.positionInfo a').text().trim()
+            let houseInfos = $(element).find('.houseInfo').text().split('|')
+            let floor = houseInfos[0].trim().replace(/\s+/g, '')
+            let size = houseInfos[1].trim()
+            let direction = houseInfos[2].trim()
+            let follow = +$(element).find('div.followInfo').text().split('/')[0].trim().split('人')[0]
+            let time = $(element).find('div.followInfo').text().split('/')[1].trim()
+            houses.push({
+                title,
+                address,
+                floor,
+                size,
+                direction,
+                follow,
+                time
+            })
+        })
+        return {
+            time: new Date().getTime(),
+            data: houses
+        }
+    }
+    public analyze(html: string, filePath: string) {
+        const houses = this.getHouseInfo(html)
+        const fileContent = this.getJSONInfos(houses, filePath)
+        return JSON.stringify(fileContent)
+    }
+}
