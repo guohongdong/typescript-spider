2c6ab6af4e2df1790b8593c1cae29dbb513267e0
hongdong <isguohongdong@gmail.com>
2020-12-31 11:43:07 +0800

    添加 cheerio 解析网页信息

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
 import superagent from 'superagent'
+import cheerio from 'cheerio'
 
+interface HouseInfo {
+    title: string,
+    follow: string
+}
 class Spider {
     private url = 'https://hz.ke.com/ershoufang/'
+    getHouseInfo(html: string) {
+        let houses: HouseInfo[] = []
+        const $ = cheerio.load(html)
+        $('li.clear').map((index, element) => {
+            let title = $(element).find('a.maidian-detail').text().trim()
+            let follow = $(element).find('div.followInfo').text().split('/')[0].trim()
+            houses.push({
+                title,
+                follow
+            })
+        })
+        return {
+            time: new Date().getTime(),
+            data: houses
+        }
+    }
     async getRawHtml() {
         let result = await superagent.get(this.url)
-        console.log(result.text)
+        this.getHouseInfo(result.text)
     }
     constructor() {
         this.getRawHtml()
