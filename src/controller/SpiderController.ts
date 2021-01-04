521216b3ce4ae76a3dac25616dc091d4b4cda2ad
hongdong <isguohongdong@gmail.com>
2021-01-04 22:16:49 +0800

    中间件装饰器的编写

---
+++ src/controller/SpiderController.ts
@@ -0,0 +1,48 @@
+import 'reflect-metadata'
+import fs from 'fs'
+import path from 'path'
+import { Request, Response, NextFunction } from 'express'
+import { get, controller, use } from './decorator'
+import { getResponseData } from '../utils/util'
+import Analyzer, { Content } from '../utils/analyzer'
+import Spider from '../utils/spider'
+
+/**
+ * 检查登录状态的中间件
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+const checkLogin = (req: Request, res: Response, next: NextFunction) => {
+    const isLogin = req.session ? req.session.login : false
+    if (isLogin) {
+        next()
+    } else {
+        res.json(getResponseData(null, '请先登录'))
+    }
+}
+
+@controller
+class SpiderController {
+    @get('/getData')
+    @use(checkLogin)
+    getData(request: Request, response: Response) {
+        const url = 'https://hz.ke.com/ershoufang/'
+        const analyze = Analyzer.getInstance()
+        new Spider(url, analyze)
+        response.json(getResponseData(true))
+    }
+
+    @get('/showData')
+    @use(checkLogin)
+    showData(request: Request, response: Response) {
+        try {
+            const filePath = path.resolve(__dirname, '../../data/house.json')
+            let fileContent: Content = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+            response.json(getResponseData(fileContent))
+        } catch (error) {
+            response.json(getResponseData(false, '尚未获取过数据'))
+        }
+    }
+}
--- src/controller/decorator.ts
+++ src/controller/decorator.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express'
+import { Router, RequestHandler } from 'express'
 export const router = Router()
 
 enum Method {
@@ -10,12 +10,23 @@ export function controller(target: any) {
     for (let key in target.prototype) {
         const path = Reflect.getMetadata('path', target.prototype, key)
         const method: Method = Reflect.getMetadata('method', target.prototype, key)
+        const middleware = Reflect.getMetadata('middleware', target.prototype, key)
         const handle = target.prototype[key]
         if (path && method && handle) {
-            router[method](path, handle)
-            router.get(path, handle)
+            if (middleware) {
+                router[method](path, middleware, handle)
+            } else {
+                router[method](path, handle)
+
+            }
         }
     }
+}
+export function use(middleware: RequestHandler) {
+    return function (target: any, key: string) {
+        Reflect.defineMetadata('middleware', middleware, target, key)
+    }
+
 }
 function getRequestDecorator(type: string) {
     return function (path: string) {
@@ -24,8 +35,8 @@ function getRequestDecorator(type: string) {
             Reflect.defineMetadata('method', type, target, key)
         }
     }
-
 }
+
 export const get = getRequestDecorator('get')
 export const post = getRequestDecorator('post')
 export const put = getRequestDecorator('put')
